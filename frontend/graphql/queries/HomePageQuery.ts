b3f23081fa401fa1c97d63d4d54ba2a78b2b2b5a
lizaq94 <domagala.k94@gmail.com>
2024-05-09 22:36:35 +0200

    Deletes old query and modifies home page query

--- frontend/graphql/queries/HomePageQuery.ts
+++ frontend/graphql/queries/HomePageQuery.ts
@@ -1,5 +1,5 @@
 export const homePageQuery = `
-    query HomePage {
+  query HomePage {
 		homePages {
 			navigation {
 				id
@@ -7,7 +7,64 @@ export const homePageQuery = `
 					label
 				}
 			}
+			heroSection {
+				title
+				description
+				button {
+					id
+				}
+				imageBackground {
+					url
+				}
+			}
 			serviceSectionTile
+			whyWeSection {
+				title
+				block {
+					id
+					title
+					description
+					image {
+						url
+					}
+				}
+			}
+			achievementSection {
+				achievementCounter {
+					id
+					number
+					description
+				}
+			}
 			projectsSectionTile
+			projectSectionButtonText
+			aboutUsSection {
+				title
+				content
+				image {
+					url
+				}
+			}
+			contactSection {
+				title
+				buttonText
+				nameInput {
+					label
+					placeholder
+				}
+				emailInput {
+					label
+					placeholder
+				}
+				phoneInput {
+					label
+					placeholder
+				}
+				messageInput {
+					label
+					placeholder
+				}
+			}
+			footerContent
 		}
 	}`;
